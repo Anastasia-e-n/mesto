fcc2a41add1d9dc4fc96f445cb7ca1bb90d8459e
Anastasia Nikiforova <nikiforovaanastasia16@gmail.com>
2023-07-04 21:41:02 +0300

    исправление ошибок

--- js/index.js
+++ js/index.js
@@ -1,36 +1,27 @@
 let editButtonElement = document.querySelector('.profile__edit-button');
 let closeButtonElement = document.querySelector('.popup__close-button');
 let popupElement = document.querySelector('.popup');
+let saveButtonElement = document.querySelector('.popup__form-button');
 let nameEditElement = document.querySelector('.profile__title');
 let jobEditElement = document.querySelector('.profile__subtitle');
-
-
-function handleClick() {
-	popupElement.classList.toggle('popup--opened');
-}
-
-editButtonElement.addEventListener('click', handleClick);
-closeButtonElement.addEventListener('click', handleClick);
-
-popupElement.addEventListener('click', (e) => {
-	if (e.target === e.currentTarget) {
-		popupElement.classList.toggle('popup--opened');
-	}
-});
-
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('#nameInput');
 let jobInput = document.querySelector('#jobInput');
 
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    handleClick();
-    nameEditElement.textContent = nameInput.value;
-    jobEditElement.textContent = jobInput.value;
+function openPopup(evt) {
+	evt.preventDefault();
+	popupElement.classList.add('popup_opened');
+	nameInput.value = nameEditElement.textContent;
+	jobInput.value = jobEditElement.textContent;
 }
-formElement.addEventListener('submit', handleFormSubmit);
 
+function closePopup(evt) {
+	evt.preventDefault();
+	popupElement.classList.remove('popup_opened');
+	nameEditElement.textContent = nameInput.value;
+   jobEditElement.textContent = jobInput.value;
+}
 
-
-
-
+editButtonElement.addEventListener('click', openPopup);
+closeButtonElement.addEventListener('click', closePopup);
+saveButtonElement.addEventListener('click', closePopup);
