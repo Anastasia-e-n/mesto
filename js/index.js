b4caae6d72d39de623c6e0ad154c4a92bfea973f
Anastasia Nikiforova <nikiforovaanastasia16@gmail.com>
2023-07-05 21:58:09 +0300

    исправление ошибок

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 let editButtonElement = document.querySelector('.profile__edit-button');
 let closeButtonElement = document.querySelector('.popup__close-button');
 let popupElement = document.querySelector('.popup');
-let saveButtonElement = document.querySelector('.popup__form-button');
 let nameEditElement = document.querySelector('.profile__title');
 let jobEditElement = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
@@ -18,10 +17,15 @@ function openPopup(evt) {
 function closePopup(evt) {
 	evt.preventDefault();
 	popupElement.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+	evt.preventDefault();
+	closePopup(evt);
 	nameEditElement.textContent = nameInput.value;
    jobEditElement.textContent = jobInput.value;
 }
 
 editButtonElement.addEventListener('click', openPopup);
 closeButtonElement.addEventListener('click', closePopup);
-saveButtonElement.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit)
